bff157a14c11340dfe794c39e226076228240dd7
Wilson Poon <84837660+wilsonpoon-github@users.noreply.github.com>
2021-06-12 15:57:24 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P8) == 0) {
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.showString("" + (pins.digitalReadPin(DigitalPin.P5)))
+})
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P5) == 0) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
     }
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
 })
