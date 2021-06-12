d6786cc66de37616ce64f056aec882b73c1ab057
Wilson Poon <84837660+wilsonpoon-github@users.noreply.github.com>
2021-06-12 15:46:14 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P8) == 0) {
         pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
     }
     pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
 })
